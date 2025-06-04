2dbf5a2d07f24a45cece8c62d0076130492d124b
jimmie_west <jimmie.west@pixleon.com>
2025-06-04 21:18:05 +0200

    Jitter fixes.

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749063432|36463831881';
+const CACHE_VERSION = '1749064651|37682676328';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
