5d9351a69762c3fd9b2a897b4fd328065b729691
jimmie_west <jimmie.west@pixleon.com>
2025-06-04 20:05:43 +0200

    Store state.

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749060094|33125671152';
+const CACHE_VERSION = '1749060294|33326443807';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
