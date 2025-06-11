f2cf191bd8a1de6049d919d8855e0b92ad988654
jimmie_west <jimmie.west@pixleon.com>
2025-06-11 13:47:51 +0200

    Fix touch.

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749641306|419645535587';
+const CACHE_VERSION = '1749642455|420794845204';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
