d9ba739f14a9b9eb414dfbb337f660c7e3a17a73
jimmie_west <jimmie.west@pixleon.com>
2025-06-11 14:53:46 +0200

    Backpack.

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749645269|423608348814';
+const CACHE_VERSION = '1749646408|424748130472';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
