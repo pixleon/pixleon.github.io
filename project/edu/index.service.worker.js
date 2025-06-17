7cec396628f60c2e53e8eccc6d86d123d2bf2120
jimmie_west <jimmie.west@pixleon.com>
2025-06-17 10:16:46 +0200

    Store island mods

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750103170|32367076';
+const CACHE_VERSION = '1750148189|7246373696';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
