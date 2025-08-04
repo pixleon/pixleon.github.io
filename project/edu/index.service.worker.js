a51fa21f06fdd1a95f910a0bd8e091ab116bdb84
jimmie_west <jimmie.west@pixleon.com>
2025-08-04 15:09:50 +0200

    Update.

--- project/edu/index.service.worker.js
+++ project/edu/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754301003|82259246680';
+const CACHE_VERSION = '1754312978|94235244523';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
