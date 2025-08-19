25927a9a3f6cf91b90d5e8f6f9118a9a858210eb
jimmie_west <jimmie.west@pixleon.com>
2025-08-19 09:43:37 +0200

    Update.

--- ahoyoo_beta/index.service.worker.js
+++ ahoyoo_beta/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755588447|2150968224';
+const CACHE_VERSION = '1755589403|3107367527';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
