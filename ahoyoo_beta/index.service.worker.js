10fd4c5c3d409084e1a8801f40443bda3455639a
jimmie_west <jimmie.west@pixleon.com>
2025-11-08 14:03:48 +0100

    Make console button hidden.

--- ahoyoo_beta/index.service.worker.js
+++ ahoyoo_beta/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762605757|3977995345';
+const CACHE_VERSION = '1762607012|5232844261';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
