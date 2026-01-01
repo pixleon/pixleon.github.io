0f11d9b2ae946e889488768a24ae575f711e35ed
jimmie_west <jimmie.west@pixleon.com>
2026-01-01 20:23:28 +0100

    Fix keyboard on IOS?

--- ahoyoo_beta/index.service.worker.js
+++ ahoyoo_beta/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767268522|262048833028';
+const CACHE_VERSION = '1767295395|288922579282';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
