3ea7e5cca999932403a6f3538bd0ecec81cc7eaa
jimmie_west <jimmie.west@pixleon.com>
2026-01-11 11:27:32 +0100

    Patch scramble island.

--- ahoyoo_beta/index.service.worker.js
+++ ahoyoo_beta/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768126844|80335483346';
+const CACHE_VERSION = '1768127235|80726450021';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahoyoo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
